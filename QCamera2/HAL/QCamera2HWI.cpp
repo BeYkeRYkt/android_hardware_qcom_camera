5f3827dbd84113332fdd586a372730329919e517
Emilian Peev <epeevs@codeaurora.org>
2016-05-19 15:40:40 +0300

    QCamera2: Release preview buffers using handles
     - The camera stream pointers passed as part of
       the preview callback arguments in no preview
       display mode could get invalidated and
       returning the preview buffers might cause
       instabilities. This is possible when the notifier
       thread gets blocked for a long time, enough
       for the preview to stop and respective channels
       to get destroyed.
       To avoid this instead of pointers to the preview
       stream the callback arguments will include a
       stream handle. When the buffer is returned back
       the handle will be used to query for the respective
       stream and avoid any possibly incorrect memory
       operations.
    
    Change-Id: I6fb877b33e0cb558a7293d7fe60eab8001f9c7ac

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -7642,12 +7642,29 @@ void QCamera2HardwareInterface::returnStreamBuffer(void *data,
                                                    void *cookie,
                                                    int32_t /*cbStatus*/)
 {
-    QCameraStream *stream = ( QCameraStream * ) cookie;
-    int idx = *((int *)data);
-    if ((NULL != stream) && (0 <= idx)) {
-        stream->bufDone((uint32_t)idx);
-    } else {
-        ALOGE("%s: Cannot return buffer %d %p", __func__, idx, cookie);
+    QCamera2HardwareInterface *pme = (QCamera2HardwareInterface *) cookie;
+    qcamera_stream_release_t *streamRelease = (qcamera_stream_release_t *) data;
+    if ((NULL != streamRelease) && (NULL != pme)) {
+        QCameraStream *stream = NULL;
+        for (size_t i = 0; i < QCAMERA_CH_TYPE_MAX; i++) {
+            if (pme->m_channels[i]) {
+                stream = pme->m_channels[i]->getStreamByHandle(
+                        streamRelease->stream_handle);
+                if (NULL != stream) {
+                    break;
+                }
+            }
+        }
+        if (NULL != stream) {
+            stream->bufDone(streamRelease->buf_idx);
+        } else {
+            ALOGE("%s: Stream with handle: %d not present!", __func__,
+                    streamRelease->stream_handle);
+        }
+    }
+
+    if (NULL != streamRelease) {
+        free(streamRelease);
     }
 }
 
