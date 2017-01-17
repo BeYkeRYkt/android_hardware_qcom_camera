b6c4f06860270e88c29b73be283aaab72397bedc
Saritha Yamasani <syamas@codeaurora.org>
2017-01-17 10:40:57 +0530

    QCamera2: Reorder the bestshot enum in Qcamera parameters
    Issue:
    Select the bestshot mode as Backlight and capture
    picture then check metering mode set in properties of
    capture image, metering mode shows Center weighted as expected.
    Exit from camera app and then capture picture with same bestshot
    mode as Backlihgt,now check metering mode in capture image.
    Metering mode shows as Average this is worng.
    This is because in stats port the parameters set by bestshot mode
    are getting overwritten since CAM_INTF_PARM_BESTSHOT_MODE enum value
    is lesser than  metering mode CAM_INTF_PARM_AEC_ALGO_TYPE.
    
    Fix:
    Made a workaround fix to make sure enum CAM_INTF_PARM_BESTSHOT_MODE
    greater than CAM_INTF_PARM_AEC_ALGO_TYPE.
    
    CRs-Fixed: 1107767
    Change-Id: Iea605ee829fc970103c7d9d9109d16a8604f4fa4

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, 2017 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1548,7 +1548,7 @@ typedef enum {
     CAM_INTF_PARM_FPS_RANGE,
     CAM_INTF_PARM_AWB_LOCK, /* 10 */
     CAM_INTF_PARM_EFFECT,
-    CAM_INTF_PARM_BESTSHOT_MODE,
+    CAM_INTF_PARM_RAW_DIMENSION,
     CAM_INTF_PARM_DIS_ENABLE,
     CAM_INTF_PARM_LED_MODE,
     CAM_INTF_META_HISTOGRAM,
@@ -1584,7 +1584,7 @@ typedef enum {
     CAM_INTF_PARM_RECORDING_HINT,
     CAM_INTF_PARM_HDR,
     CAM_INTF_PARM_MAX_DIMENSION,
-    CAM_INTF_PARM_RAW_DIMENSION,
+    CAM_INTF_PARM_BESTSHOT_MODE,
     CAM_INTF_PARM_FRAMESKIP,
     CAM_INTF_PARM_ZSL_MODE,  /* indicating if it's running in ZSL mode */
     CAM_INTF_PARM_BURST_NUM,
