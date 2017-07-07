27d8c38f6638a842e1c7241104bb0928cb6bfdb3
Chandan Gera <cgera@codeaurora.org>
2017-07-07 17:57:06 +0530

    QCamera2: HAL3: Use CAM_TYPE_STANDALONE for RAW
    --If the camera is in RAW only mode, use it as
    STANDALONE camera and not MAIN camera
    
    Change-Id: Ie0cbef724f76cb3f7a2327ea3b81f42fea52c94a

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -2420,6 +2420,9 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
             LOGW("Analysis channel cannot be created");
         }
     }
+    if (onlyRaw) {
+        mStreamConfigInfo.sync_type = CAM_TYPE_STANDALONE;
+    }
 
     //RAW DUMP channel
     if (mEnableRawDump && isRawStreamRequested == false){
