44a135e9f7cc7b2d2ef9921ddf85528fd04ca0d4
Yong Zhang <zyong@codeaurora.org>
2017-08-24 10:51:26 +0800

    QCamera2/HAL3: Add mutex unlock.
    Change-Id: I8e74f0f748c6e72d0f84a841a874f70f6cf89483
    Signed-off-by: Yong Zhang <zyong@codeaurora.org>

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -3569,6 +3569,7 @@ no_error:
             if(ADD_SET_PARAM_ENTRY_TO_BATCH(mParameters,
                 CAM_INTF_META_FRAME_NUMBER, request->frame_number)) {
                 ALOGE("%s: Failed to set the frame number in the parameters", __func__);
+                pthread_mutex_unlock(&mMutex);
                 return BAD_VALUE;
             }
         }
