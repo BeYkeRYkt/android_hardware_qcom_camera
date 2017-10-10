9e05abbbf287e511d6bc5cacf3a65cc9d874a65a
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2017-10-10 21:28:27 +0530

    QCamera2: Add support for bokeh snapshot with digital zoom.
    Issue: Zoom is not allowed when Bokeh feature is enabled.
    
    Fix:
    1. Add support for bokeh snapshot with digital zoom.
    2. Add support for encoding depth map instead of YUV.
    3. Add new dual camera configuration macro to be changed
       according to the requirement.
    
       Following property can also be used to change it runtime:
       adb shell setprop persist.camera.ddm.config <value>
       Value has to be in sync with the ddm lib header.
    
    Change-Id: I3b287b076bcde0f03aa47593f1d5a714601381ce

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -11852,6 +11852,8 @@ void QCamera2HardwareInterface::configureSnapshotSkip(bool skip)
                     }
                 }
             }
+            if (skip)
+                ((QCameraPicChannel *)pChannel)->flushSuperbuffer(mActiveCameras, 0);
         }
     }
 }
